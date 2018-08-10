14baa8f40d7cafca32bb50566cfb3bd7d6f07830
szwiec <jakub.szwiec@navenio.com>
2018-08-10 17:00:58 +0100

    info fragment

---
+++ app/src/main/java/eu/szwiec/checkittravelkit/InfoFragment.kt
@@ -0,0 +1,15 @@
+package eu.szwiec.checkittravelkit
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class InfoFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_info, container, false)
+    }
+}
