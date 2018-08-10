0f1d9d641a033fc90c6d1ac8055e613651dc48c9
szwiec <jakub.szwiec@navenio.com>
2018-08-10 12:14:39 +0100

    migrate to AndroidX

--- app/src/main/java/eu/szwiec/checkittravelkit/MainActivity.kt
+++ app/src/main/java/eu/szwiec/checkittravelkit/MainActivity.kt
@@ -1,7 +1,7 @@
 package eu.szwiec.checkittravelkit
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
--- app/src/main/java/eu/szwiec/checkittravelkit/SplashFragment.kt
+++ app/src/main/java/eu/szwiec/checkittravelkit/SplashFragment.kt
@@ -1,12 +1,10 @@
 package eu.szwiec.checkittravelkit
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 
 // TODO: Rename parameter arguments, choose names that match
