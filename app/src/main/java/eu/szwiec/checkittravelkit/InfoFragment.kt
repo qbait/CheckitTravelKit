885a62c032bc2df9bfde3b51f18fd57d6bc4d93e
szwiec <jakub.szwiec@navenio.com>
2018-08-10 18:33:37 +0100

    pass args

--- app/src/main/java/eu/szwiec/checkittravelkit/InfoFragment.kt
+++ app/src/main/java/eu/szwiec/checkittravelkit/InfoFragment.kt
@@ -5,11 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_info.*
 
 class InfoFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_info, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val id = InfoFragmentArgs.fromBundle(arguments).id
+        tv.text = id
     }
 }
--- app/src/main/java/eu/szwiec/checkittravelkit/SearchFragment.kt
+++ app/src/main/java/eu/szwiec/checkittravelkit/SearchFragment.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.fragment_search.*
 
 class SearchFragment : Fragment() {
@@ -19,7 +19,9 @@ class SearchFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         button.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.next_action)
+            val action = SearchFragmentDirections.nextAction()
+            action.setId("PL")
+            view.findNavController().navigate(action)
         }
     }
 }
