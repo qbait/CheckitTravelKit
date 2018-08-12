ba62e2e065b37d92cce1b03317e4358747e823b3
szwiec <jakub.szwiec@navenio.com>
2018-08-12 19:20:08 +0100

    new dependencies

--- app/src/main/java/eu/szwiec/checkittravelkit/di/app_module.kt
+++ app/src/main/java/eu/szwiec/checkittravelkit/di/app_module.kt
@@ -1,12 +1,18 @@
 package eu.szwiec.checkittravelkit.di
 
 import eu.szwiec.checkittravelkit.prefs.Preferences
+import eu.szwiec.checkittravelkit.repository.CountryRepository
+import eu.szwiec.checkittravelkit.ui.search.SearchViewModel
 import eu.szwiec.checkittravelkit.ui.splash.SplashViewModel
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 
 
 val appModule = module {
     viewModel { SplashViewModel(get()) }
+    viewModel { SearchViewModel(get(), get(), get()) }
     single { Preferences(get(), "prefs") }
+    single { CountryRepository() }
+    single { androidContext().resources }
 }
