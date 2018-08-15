f84964f2c99494c7b77c432c2b0c4f5378e304f6
szwiec <jakub.szwiec@navenio.com>
2018-08-15 11:38:05 +0100

    add Embedded annotation

--- app/src/main/java/eu/szwiec/checkittravelkit/vo/Country.kt
+++ app/src/main/java/eu/szwiec/checkittravelkit/vo/Country.kt
@@ -1,5 +1,6 @@
 package eu.szwiec.checkittravelkit.vo
 
+import androidx.room.Embedded
 import androidx.room.Entity
 import com.squareup.moshi.Json
 
@@ -8,24 +9,37 @@ import com.squareup.moshi.Json
 @Entity(primaryKeys = arrayOf("name"))
 //@TypeConverters(Converters::class)
 data class Country(
+
         @Json(name = "id")
         val id: String,
+
         @Json(name = "name")
         val name: String,
+
         @Json(name = "timezone")
         val timezone: String = "",
+
+        @Embedded
         @Json(name = "electricity")
         val electricity: Electricity = Electricity(),
+
+        @Embedded
         @Json(name = "call_info")
         val callInfo: CallInfo = CallInfo(),
+
         @Json(name = "tap_water")
         val tapWater: String = "",
+
         @Json(name = "vaccinations")
         val vaccinations: Map<String, String> = emptyMap(),
+
+        @Embedded
         @Json(name = "currency")
         val currency: Currency = Currency(),
+
         @Json(name = "image_url")
         val imageUrl: String = "",
+
         @Json(name = "visa")
         var visa: String = ""
 )
