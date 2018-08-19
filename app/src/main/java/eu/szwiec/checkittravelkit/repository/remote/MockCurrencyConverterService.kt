37bb2e8b88334f05f44db6a18714e01fb1843c04
szwiec <jakub.szwiec@navenio.com>
2018-08-19 20:27:53 +0100

    delay for MockCurrencyConverter

--- app/src/main/java/eu/szwiec/checkittravelkit/repository/remote/MockCurrencyConverterService.kt
+++ app/src/main/java/eu/szwiec/checkittravelkit/repository/remote/MockCurrencyConverterService.kt
@@ -1,5 +1,6 @@
 package eu.szwiec.checkittravelkit.repository.remote
 
+import android.os.Handler
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import eu.szwiec.checkittravelkit.repository.data.Rate
@@ -8,12 +9,13 @@ import retrofit2.Response
 class MockCurrencyConverterService : CurrencyConverterService {
 
     override fun convert(from_to: String, compact: String): LiveData<ApiResponse<Map<String, Rate>>> {
-        val liveData = MutableLiveData<ApiResponse<Map<String, Rate>>>()
+        val currencyLD = MutableLiveData<ApiResponse<Map<String, Rate>>>()
         val rate = Rate(2.5f)
         val map = mapOf("XXX_XXX" to rate)
         var apiResponse = ApiResponse(Response.success<Map<String, Rate>>(map))
-        liveData.value = apiResponse
 
-        return liveData
+        Handler().postDelayed({ currencyLD.postValue(apiResponse) }, 500)
+
+        return currencyLD
     }
 }
