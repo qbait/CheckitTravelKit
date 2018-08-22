6dd3be5d4b44535af67ef454bd0754ca52dcd920
szwiec <jakub.szwiec@navenio.com>
2018-08-22 19:15:09 +0100

    plant Timber

--- app/src/main/java/eu/szwiec/checkittravelkit/App.kt
+++ app/src/main/java/eu/szwiec/checkittravelkit/App.kt
@@ -6,10 +6,12 @@ import eu.szwiec.checkittravelkit.di.appModule
 import eu.szwiec.checkittravelkit.di.localDatasourceModule
 import eu.szwiec.checkittravelkit.di.mockRemoteDataSourceModule
 import org.koin.android.ext.android.startKoin
+import timber.log.Timber
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        Timber.plant(Timber.DebugTree())
         startKoin(this, listOf(appModule, mockRemoteDataSourceModule, localDatasourceModule))
         AndroidThreeTen.init(this)
     }
