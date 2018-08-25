bc3130bc07519a1ddb58da9e6fd9308c598050fd
szwiec <jakub.szwiec@navenio.com>
2018-08-25 12:03:48 +0100

    remove unused model

--- app/src/main/java/eu/szwiec/checkittravelkit/repository/data/Textual.kt
+++
@@ -1,8 +0,0 @@
-package eu.szwiec.checkittravelkit.repository.data
-
-import com.squareup.moshi.Json
-
-data class Textual(
-        @Json(name = "class") val classX: String,
-        @Json(name = "text") val text: List<String>
-)
